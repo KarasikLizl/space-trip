154b6d16c7a1820703393c22bbef8801f9849780
KarasikLizl <braginskayarita@gmail.com>
2021-08-13 06:41:39 +0300

    winScene

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,11 @@ import { BootScene } from './scene/Boot/boot';
 import { PreloadScene } from './scene/Preload/preload';
 import { MobileError } from './scene/MobileError/mobileError';
 import { globalSettings } from './settings';
+import { WinScene } from './scene/Win/win';
 
 const config: Phaser.Types.Core.GameConfig = {
     ...globalSettings,
-    scene: [BootScene, PreloadScene, MenuScene, GameScene, EndScene, MobileError],
+    scene: [BootScene, PreloadScene, MenuScene, GameScene, WinScene, EndScene, MobileError],
 };
 
 new Phaser.Game(config);
---
+++ src/scene/Win/winsettings.ts
@@ -0,0 +1,20 @@
+export const winSettings =  {
+    player:{
+        startPositionX: 20,
+        startPositionY: 20,
+        minSpeed: 25,
+        maxSpeed: 50,
+    },
+    ui: {
+        title: {
+            fontSize: 100,
+            fontColor: '#fff',
+            text: 'Ого, ты выиграл!',
+        },
+        description: {
+            fontSize: 25,
+            fontColor: '#fff',
+            text: '( нажмите в любое место, что бы начать заново(можно не надо...) )',
+        }
+    }
+}
